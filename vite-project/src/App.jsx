f6c7b6ac03e6ef64aec2e8ac4d517db6ac87981f
Aayush Chhuka <aayushchhuka7@gmail.com>
2026-02-09 11:44:43 +0545

    useState()

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,88 +1,23 @@
-// export default function App() {
-//   const user_name = "Aayush";
-//   const age = 21;
-//   return (
-//     <>
-//       <button onClick={() => console.log("hell")}>hell</button>
-//       <label htmlFor="email">Email:</label>
-//       <input id="email" type="email" />
-//       <div>
-//         <h1>Hello, {user_name}!</h1>
-//         <h2>You are {age} years old.</h2>
-//         <h2>{age > 18 ? "You are an adult." : "You are a minor."}</h2>
-//         <Button />
-//         <Button />
-//         <Button />
-//         <Button />
-//       </div>
-//     </>
-//   );
-// }
+import React, { useState } from "react";
 
-// import React from 'react'
-
-// export default function App() {
-//   return (
-//     <div>App</div>
-//   )
-// }
-
-// import React from "react";
-
-// export const App = () => {
-//   return <div>App</div>;
-// };
-// export default App;
-
-// import Product from "./components/ProductCard";
-// export const App = () => {
-//   const laptop = [
-//     {
-//       brand: "Dell",
-//       price: 800,
-//       image: "./images/lenovo.webp",
-//     },
-//     {
-//       brand: "Apple",
-//       price: 1200,
-//       image: "./images/mac.jpg",
-//     },
-//     {
-//       brand: "HP",
-//       price: 700,
-//       image: "./images/hp.avif",
-//     },
-//   ];
-
-//   return (
-//     <>
-//       {/* <Product name={laptop[0].brand} price={laptop[0].price} />
-//       <Product name={laptop[1].brand} price={laptop[1].price} />
-//       <Product name={laptop[2].brand} price={laptop[2].price} /> */}
-
-//       {laptop.map((lap, i) => (
-//         <Product key={i} {...lap} />
-//       ))}
-//     </>
-//   );
-// };
-
-import Blog from "./components/Blog";
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [isloggedIn, setIsLoggedIn] = useState(false);
+  function handleIncrement() {
+    setCount((oldcount) => oldcount + 1);
+    setCount((oldcount) => oldcount + 1);
+    setCount((oldcount) => oldcount + 1);
+  }
   return (
     <div>
-      <h2>Blog Details</h2>
-      <Blog>
-        <h3>Blog Title 1</h3>
-        <p>Blog Description</p>
-      </Blog>
+      <h3>{count}</h3>
+      <button onClick={handleIncrement}>Increment</button>
+      {isloggedIn && <h3>Welcome</h3>}
+      {isloggedIn && <h3>Please Login in</h3>}
 
-      <Blog>
-        <h3>Blog Title 2</h3>
-        <p>Blog Description</p>
-      </Blog>
+      <button onClick={() => setIsLoggedIn(!isloggedIn)}>
+        {isloggedIn ? "LogOut" : "Login"}
+      </button>
     </div>
   );
 }
-
-export default App;
